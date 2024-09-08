2aead15f045e8ee9c2243719c3097e386e9eb030
Tutu Moses <etu.moses@gmail.com>
2024-09-08 05:48:34 +0000

    1

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+   with open("requirements.txt") as f:
+       install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in church_contributions/__init__.py
-from church_contributions import __version__ as version
-
-setup(
-    name="church_contributions",
-    version=version,
-    description="Church Contribution System for Anglican Church",
-    author="Tutu Moses",
-    author_email="etu.moses@gmail.com",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires
-)
+   setup(
+       name="church_contributions",
+       version='0.0.1',
+       description="Church Contribution System for Anglican Church",
+       author="Tutu Moses",
+       author_email="etu.moses@gmail.com",
+       packages=find_packages(),
+       zip_safe=False,
+       include_package_data=True,
+       install_requires=install_requires
+   )
