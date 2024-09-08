64ac194473c15c31eca5517bb0a764d3d56bec4c
Tutu Moses <etu.moses@gmail.com>
2024-09-08 05:50:40 +0000

    2

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-   with open("requirements.txt") as f:
-       install_requires = f.read().strip().split("\n")
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
-   setup(
-       name="church_contributions",
-       version='0.0.1',
-       description="Church Contribution System for Anglican Church",
-       author="Tutu Moses",
-       author_email="etu.moses@gmail.com",
-       packages=find_packages(),
-       zip_safe=False,
-       include_package_data=True,
-       install_requires=install_requires
-   )
+setup(
+    name="church_contributions",
+    version='0.0.1',
+    description="Church Contribution System for Anglican Church",
+    author="Tutu Moses",
+    author_email="etu.moses@gmail.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
+)
